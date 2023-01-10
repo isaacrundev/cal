39f604fcef5b9e36aef4480a16f2770f708e8825
Isaac <shocker945516@gmail.com>
2023-01-10 11:36:50 -0800

    first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,39 +3,77 @@ import { CssBaseline, AppBar, Toolbar, Typography, Box } from "@mui/material";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import CalculateIcon from "@mui/icons-material/Calculate";
-import { Link } from "react-router-dom";
-import { Height } from "@mui/icons-material";
+import TdeeCal from "../pages/TdeeCal";
+// import { Link } from "react-router-dom";
 
 export default function Header() {
+  const clickHandler = () => {
+    return <TdeeCal />;
+  };
   return (
     <>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <Box>
-              <Link to="/">
-                <Typography variant="h4">
-                  <FitnessCenterIcon fontSize="large" />
-                  Website Title
-                </Typography>
-              </Link>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              width: "100%",
+              maxWidth: "1366px",
+              margin: "0 auto",
+            }}
+          >
+            <Box sx={{ width: "100%" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                {/* <Link to="/"> */}
+                <FitnessCenterIcon />
+                <Typography variant="h5">Website Title</Typography>
+                {/* </Link> */}
+              </Box>
             </Box>
-            <Box>
-              <Link to="/dashboard">
-                <Typography variant="h6">
-                  <DashboardIcon />
-                  Dashboard
-                </Typography>
-              </Link>
-            </Box>
-            <Box>
-              <Link to="/tdeecal">
-                <Typography variant="h6">
-                  <CalculateIcon />
-                  TDEE Calculator
-                </Typography>
-              </Link>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: "10px",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                {/* <Link to="/dashboard"> */}
+                <DashboardIcon fontSize="small" />
+                <Typography variant="subtitle1">Dashboard</Typography>
+                {/* </Link> */}
+              </Box>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <button onClick={clickHandler}>
+                  {/* <Link to="/tdeecal"> */}
+                  <CalculateIcon fontSize="small" />
+                  <Typography variant="subtitle1">Calculator</Typography>
+                </button>
+                {/* </Link> */}
+              </Box>
             </Box>
           </Box>
         </Toolbar>
